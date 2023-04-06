0063b8b46b16fc60cc61060410a3509865f6dca7
kaldate <101926618+kaldate@users.noreply.github.com>
2023-04-07 02:11:57 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports ={
+   //MONGO_URI:"mongodb+srv://mahen:12345a@cluster0.qfgbe.mongodb.net/E-voting?retryWrites=true&w=majority",
+    MONGO_URI:"mongodb+srv://kaldatepriya33:512512p+@cluster0.4fndyez.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"MOHENMONDALJDFKFLL"
+
+}
